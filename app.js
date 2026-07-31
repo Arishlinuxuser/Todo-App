beb4cddf96eb0f614f3e41152e0ac92e34c2ac26
Arishlinuxuser <a8493402@gmail.com>
2026-07-31 14:47:45 +0530

    Initial Todo App project

---
+++ app.js
@@ -0,0 +1,50 @@
+let body = document.querySelector("body");
+let input = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+let ul = document.querySelector("ul");
+let warning = document.querySelector(".warning");
+let deleteBtn = document.querySelector(".del-btn");
+let deleteBtnAll = document.querySelectorAll(".del-btn");
+
+btn.addEventListener("click", function (event) {
+  event.stopPropagation();
+  if (input.value.trim() === "") {
+    getWar();
+  } else {
+    listSty();
+    let task = document.createElement("li");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("del-btn");
+    deleteBtn.innerText = "delete";
+    task.innerText = input.value.trim();
+    task.append(deleteBtn);
+    ul.append(task);
+
+    input.value = "";
+  }
+});
+
+function listSty() {
+  if (ul.style.padding == "0px") {
+    ul.style.padding = "20px";
+  }
+}
+
+function getWar() {
+  warning.innerText = "Please Enter a Task first";
+  warning.classList.add("show");
+  setTimeout(() => {
+    warning.innerText = "";
+    warning.classList.remove("show");
+  }, 1000);
+}
+
+ul.addEventListener("click", function (event) {
+  if (event.target.classList.contains("del-btn")) {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+  }
+  if (ul.children.length <= 0) {
+    ul.style.padding = "0px";
+  }
+});
